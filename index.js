2eb91831f9e06c87aa13fc50aaf078ccbd7451e0
taku10101 <taku1011ht@gmail.com>
2023-12-20 21:20:37 +0900

    firrst commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log(`
+   ╭─────────────────────────────────────────────────────────────────────────────────╮
+   │                                                                                 │
+   │                                                                                 │
+   │                                                                                 │
+   │            Work: HAL_Nagoya                                                     |
+   |        frontend: React/Next                                                     │
+   │         backend: ruby,go                                                        │
+   |   infrastructure: home server                                                   │
+   │                                                                                 │
+   │          Zakończenie studiów w 2026 r.                                          │
+   │                                                                                 │
+   │             X:  https://x.com/tak__it                                           │
+   │            npm:  https://npmjs.com/                                             │
+   │         GitHub:  https://github.com/taku10101                                   │
+   │          Qiita: == https://qiita.com/taku10101                                  │
+   │                                                                                 │
+   │                                                                                 │
+   ╰─────────────────────────────────────────────────────────────────────────────────╯
+
+`);
